f9f981ab37a82fd73164f01f10bacbf4fac6089e
Oliver Hansen <s194591@dtu.dk>
2026-01-09 13:47:28 +0100

    Model instantiation

--- src/mlops_project/model.py
+++ src/mlops_project/model.py
@@ -1,16 +1,15 @@
-from torch import nn
-import torch
-
-class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(1, 1)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+import os
+from sentence_transformers import SentenceTransformer
+class Model:
+    def __init__(self, from_checkpoint: bool = False, cache_dir: str = 'models/cache/'):
+        os.makedirs(cache_dir, exist_ok=True)
+        if from_checkpoint:
+            raise NotImplementedError("Loading from checkpoint is not implemented yet.")
+        self.model = SentenceTransformer('all-MiniLM-L6-v2', cache_folder=cache_dir)
+    def __call__(self, x):
+        return self.model.encode(x)
 
 if __name__ == "__main__":
     model = Model()
-    x = torch.rand(1)
+    x = ["This is a test sentence.", "This is another sentence."]
     print(f"Output shape of model: {model(x).shape}")
