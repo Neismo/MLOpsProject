08441697a50f75c8ecd7107839db10cde0a4a4fc
Oliver Hansen <s194591@dtu.dk>
2026-01-09 13:50:03 +0100

    No class abstraction

--- src/mlops_project/model.py
+++ src/mlops_project/model.py
@@ -1,15 +1,14 @@
 import os
 from sentence_transformers import SentenceTransformer
-class Model:
-    def __init__(self, from_checkpoint: bool = False, cache_dir: str = 'models/cache/'):
-        os.makedirs(cache_dir, exist_ok=True)
-        if from_checkpoint:
-            raise NotImplementedError("Loading from checkpoint is not implemented yet.")
-        self.model = SentenceTransformer('all-MiniLM-L6-v2', cache_folder=cache_dir)
-    def __call__(self, x):
-        return self.model.encode(x)
+
+def instantiate_sentence_transformer(from_checkpoint: bool = False, cache_dir: str = 'models/cache/'):
+    os.makedirs(cache_dir, exist_ok=True)
+    if from_checkpoint:
+        raise NotImplementedError("Loading from checkpoint is not implemented yet.")
+    model = SentenceTransformer('all-MiniLM-L6-v2', cache_folder=cache_dir)
+    return model
 
 if __name__ == "__main__":
-    model = Model()
+    model = instantiate_sentence_transformer()
     x = ["This is a test sentence.", "This is another sentence."]
     print(f"Output shape of model: {model(x).shape}")
