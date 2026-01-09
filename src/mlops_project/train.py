59b03690313f048f262c3241f0f939f6b1f70c9a
Oliver Hansen <s194591@dtu.dk>
2026-01-09 14:10:36 +0100

    Hydra and train barebones setup

--- src/mlops_project/train.py
+++ src/mlops_project/train.py
@@ -1,10 +1,24 @@
-from mlops_project.model import Model
-from mlops_project.data import MyDataset
+import sys
+import hydra
+from pathlib import Path
+from hydra.utils import get_original_cwd
+from loguru import logger
+from mlops_project.model import instantiate_sentence_transformer as get_model
+from mlops_project.data import ArxivPapersDataset
 
-def train():
-    dataset = MyDataset("data/raw")
-    model = Model()
-    # add rest of your training code here
+# setup loggers
+logger.remove()
+logger.add(sys.stdout, level="DEBUG")
+
+@hydra.main(version_base="1.3", config_path="../../configs", config_name="train_config")
+def train(config):
+    logger.info("Starting training process...")
+    logger.debug(f"Training configuration: {config}")
+
+    train_dataset = ArxivPapersDataset("train", data_dir=Path(f"{get_original_cwd()}/data"))
+    model = get_model(cache_dir=f"{get_original_cwd()}/models/cache/")
+
+    # rest of training loop here!
 
 if __name__ == "__main__":
     train()
